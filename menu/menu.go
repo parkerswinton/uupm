c98420caa462d09f45a9ac71260f522c9fe0e938
Parker Swinton <parkerdswinton@gmail.com>
2025-11-03 17:05:25 -0800

    feat: menu full-screen

--- menu/menu.go
+++ menu/menu.go
@@ -10,6 +10,7 @@ type ProgramStatus int
 const (
 	testView ProgramStatus = iota
 	statsView
+	optionsView
 )
 
 const banner = ` _   _ _   _ ____  ____
@@ -17,6 +18,8 @@ const banner = ` _   _ _   _ ____  ____
 | |_| | |_| | |_| | | | |
 |____/|____/|  __/|_|_|_|
             |_|          `
+
+const Margin = 4
 
 type item struct {
 	title         string
@@ -31,19 +34,22 @@ type model struct {
 }
 
 type style struct {
-	base   lipgloss.Style
-	active lipgloss.Style
+	width     int
+	height    int
+	primary   lipgloss.Color
+	secondary lipgloss.Color
 }
 
 func New() model {
 	options := []item{
 		{title: "test", programStatus: testView},
 		{title: "stats", programStatus: statsView},
+		{title: "options", programStatus: optionsView},
 	}
 
 	return model{options: options, activeOption: 0, style: style{
-		base:   lipgloss.NewStyle().Bold(false).Foreground(lipgloss.Color("#7aa2f7")),
-		active: lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#4fd6be")),
+		primary:   lipgloss.Color("#7aa2f7"),
+		secondary: lipgloss.Color("#4fd6be"),
 	}}
 }
 
@@ -74,18 +80,33 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "enter":
 			m.selected = m.options[m.activeOption]
 		}
+	case tea.WindowSizeMsg:
+		m.style.height = msg.Height
+		m.style.width = msg.Width
 	}
 	return m, cmd
 }
 
 func (m model) View() string {
-	lines := []string{m.style.base.Render(banner)}
+
+	container := lipgloss.NewStyle().
+		Width(m.style.width-Margin).
+		Height(m.style.height-Margin).
+		Align(lipgloss.Center, lipgloss.Center).
+		Border(lipgloss.DoubleBorder()).
+		BorderForeground(m.style.primary)
+
+	content := lipgloss.NewStyle().Foreground(m.style.primary)
+	hovered := lipgloss.NewStyle().Foreground(m.style.secondary).Bold(true)
+
+	lines := []string{content.Render(banner)}
 	for i, option := range m.options {
 		if i == m.activeOption {
-			lines = append(lines, m.style.active.Render("> "+option.title))
+			lines = append(lines, hovered.Render("> "+option.title))
 		} else {
-			lines = append(lines, m.style.base.Render(option.title))
+			lines = append(lines, content.Render(option.title))
 		}
 	}
-	return lipgloss.JoinVertical(lipgloss.Left, lines...)
+
+	return container.Render(lipgloss.JoinVertical(lipgloss.Left, lines...))
 }
